66d4ad748c42bceddd33896c935ad15a5d90bec6
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-03-16 23:07:56 +0530

    [fix]Added catch statement to the getProductData

--- lucid-book-store/src/utility/api-call.jsx
+++ lucid-book-store/src/utility/api-call.jsx
@@ -4,7 +4,9 @@ const getProductsData = async (dispatch) => {
   try {
     const response = await axios.get("/api/products");
     dispatch({ type: "API_DATA", value: response.data.products });
-  } catch {}
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export { getProductsData };
